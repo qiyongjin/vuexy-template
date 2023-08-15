28915172922c314fa81ebc1ae739b2daf43a8ba6
qiyongjin <3131471367@qq.com>
2023-08-15 11:40:52 +0800

    create template :sparkles:

---
+++ src/pages/home/MyHome.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <h1>欢迎使用vuexy</h1>
+  </div>
+</template>
+
+<style lang="scss">
+.home {
+  color: #333;
+  margin-block: auto;
+  margin-inline: auto;
+}
+</style>
